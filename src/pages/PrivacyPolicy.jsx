241d5f838ba065983683b39f436301bded05eb95
jericyabyabin <jericmatthew.yabyabin.cics@ust.edu.ph>
2024-03-22 15:14:42 +0800

    for privacy policy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -6,30 +6,31 @@ const PrivacyPolicy = () => {
         <Helmet>
             <title>CAZA | Privacy Policy</title>
         </Helmet>
-        <main className="md:p-14 p-16 bg-white">
+        <main className="md:p-10 p-16 bg-white">
             <div>
                 <h1 className="text-4xl text-sky-950 font-bold">Privacy Policy</h1>
             </div>
                 <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-sky-900"></hr>
 
                 <h1 className="mt-24 text-3xl text-sky-950 font-semibold">Lorem, ipsum.</h1>
-                    <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
-                        <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
+                        <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
+                    <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
+               
                 <h1 className="mt-24 text-3xl text-sky-950 font-semibold">Lorem, ipsum.</h1>
-                    <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
-                <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
+                        <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
+                    <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
 
                 <h1 className="mt-24 text-3xl text-sky-950 font-semibold">Lorem, ipsum.</h1>
-                    <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
-                <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
+                        <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
+                    <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
 
                 <h1 className="mt-24 text-3xl text-sky-950 font-semibold">Lorem, ipsum.</h1>
-                    <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
-                <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
+                        <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
+                    <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
 
                 <h1 className="mt-24 text-3xl text-sky-950 font-semibold">Lorem, ipsum.</h1>
-                    <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
-                <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
+                        <hr className="w-16 h-1 my-1 bg-gray-100 border-0 rounded md:my-1 dark:bg-sky-500"></hr>
+                    <p className="mt-7 text-lg text-sky-950 font-medium">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam distinctio nemo corporis iste excepturi dicta officiis. Inventore delectus voluptatibus consequatur?</p>
         </main>
         </>
     )
